39d898ed27fb3240b1410344a267b92b18d848b1
David Jesus <62025422+davidjss04@users.noreply.github.com>
2022-11-04 11:40:38 -0500

    Added database

---
+++ api/src/loadDB/loadDiet.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const axios = require('axios');
+const { Diet, Recipe } = require('../db');
+const json = require('../hardcode.json');
+const router = Router();
+
+const getDiet = async () => {
+    json.forEach(async (e) => {
+        
+    });
+};
---
+++ api/src/loadDB/loadRecipe.js
@@ -0,0 +1,34 @@
+const { Router } = require('express');
+const axios = require('axios');
+const { Diet, Recipe } = require('../db');
+const json = require('../hardcode.json');
+const router = Router();
+
+const getRecipe = async () => {
+	json.forEach(async (e) => {
+		let recipe = await Recipe.findOne({
+			where: {
+				title: e.title,
+			},
+		});
+
+		if (!recipe) {
+			recipe = await Recipe.create({
+				title: e.title,
+				summary: e.summary,
+				healthScore: e.healthScore,
+				steps:
+					e.analyzedInstructions[0] && e.analyzedInstructions[0].steps
+						? e.analyzedInstructions[0].steps
+								.map((item) => item.step)
+								.join(' \n')
+						: '',
+				image: e.image,
+			});
+		}
+	});
+};
+
+module.exports = {
+	getRecipe,
+};
