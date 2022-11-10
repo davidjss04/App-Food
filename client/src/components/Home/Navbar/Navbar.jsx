b56fb463450887ccdc3674773844d22f84d8eef2
David Huaricancha <davidjss.dev@gmail.com>
2022-11-10 13:00:53 -0500

    prueba desde bash

--- client/src/components/Home/Navbar/Navbar.jsx
+++ client/src/components/Home/Navbar/Navbar.jsx
@@ -10,6 +10,7 @@ import styles from './Navbar.module.css';
 const Navbar = (props) => {
 	const dispatch = useDispatch();
 	const recipes = useSelector((state) => state.recipe.recipes);
+	
 	const diets = useSelector((state) => state.diet.diets);
 
 	const [search, setSearch] = useState('');
@@ -19,6 +20,7 @@ const Navbar = (props) => {
 
 	useEffect(() => {
 		dispatch(actionsDiet.getDiet());
+
 		console.log('useEffect para diets');
 	}, [dispatch]);
 
