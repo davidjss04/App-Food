6a904a0ff5519005528ddad2964173a45d6295db
David Huaricancha <davidjss.dev@gmail.com>
2022-11-15 17:49:12 -0500

    Ok refactor

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,1 @@
-export { default as LandingPage } from "./LandingPage/LandingPage";
-export { default as Home } from "./Home/Home";
-export { default as Detail } from "./Detail/Detail";
-export { default as Form } from "./Form/Form";
 export { default as Card } from "./Card/Card";
--- client/src/sections/Cards.jsx
+++ client/src/sections/Cards.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
+import { useSelector } from "react-redux";
+import { Card } from "../components";
+import {
+  selectAllRecipes,
+  getRecipesLoading,
+  getRecipesError,
+} from "../features/recipes/recipesSlice.js";
+
 const Cards = () => {
-  return <div>Cards</div>;
+  const recipes = useSelector(selectAllRecipes);
+  const loading = useSelector(getRecipesLoading);
+  const error = useSelector(getRecipesError);
+
+  if (loading || error.lenght > 0) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      {recipes.map((recipe) => (
+        <Card
+          key={recipe.id}
+          title={recipe.title}
+          summary={recipe.summary}
+          healthScore={recipe.healthScore}
+          steps={recipe.steps}
+          image={recipe.image}
+        />
+      ))}
+    </>
+  );
 };
 
 export default Cards;
